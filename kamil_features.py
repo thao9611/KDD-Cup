11faff32c38f34cf3245b2211ba906a49901459d
kvtoraman <kvtoraman@gmail.com>
2017-12-01 23:07:37 +0900

    Add to kamil's features

--- kamil_features.py
+++ kamil_features.py
@@ -1,9 +1,22 @@
-def kamil_new_f1(dataset, author_paper_pairs):
+def kamil_feature_11(dataset, author_paper_pairs):
     feature_dict = {}
+    pa_data = dataset["paper_author"]
 
-    #aid_to_count = dataset['paper_author']['AuthorId'].value_counts()
-    print(author_paper_pairs[0])
-    for ap_pair in author_paper_pairs:
-        feature_dict[ap_pair] = int(ap_pair[0]) + int(ap_pair[1])
-
+    print("Welcome to Kamil's feature!")
+    for ap in author_paper_pairs:
+        pa = ap[::-1]
+        #print("PA pair",pa)
+        paper_aff = pa_data[(pa_data["AuthorId"] == pa[1]) & (pa_data["PaperId"] == pa[0])]["Affiliation"].unique()
+        #print("Paper aff--",paper_aff,"--")
+        
+        ta_aff = " ".join(pa_data[pa_data["AuthorId"] == pa[1]]["Affiliation"].unique())
+        #print("Target aff--",ta_aff,"--")
+        coa_aff = get_coauthor_aff(pa_data, pa[0])
+        value = 0
+        for i,aff in enumerate(coa_aff):
+            if(aff != ""):
+         #       print(i,"Co auth aff::",aff,"::")
+                value += lcs(aff, ta_aff)
+        feature_dict[pa] = value
+        p#rint(pa," has ",value)
     return feature_dict
