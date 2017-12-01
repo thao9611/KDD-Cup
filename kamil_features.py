8a3e2f996a236a17f5c65ee1b3099b7cb93a6ebb
kvtoraman <kvtoraman@gmail.com>
2017-12-01 23:46:02 +0900

    add helpoer function

--- kamil_features.py
+++ kamil_features.py
@@ -1,3 +1,31 @@
+
+def lcs(X , Y):
+    # find the length of the strings
+    m = len(X)
+    n = len(Y)
+ 
+    # declaring the array for storing the dp values
+    L = np.zeros((m+1,n+1))
+ 
+    """Following steps build L[m+1][n+1] in bottom up fashion
+    Note: L[i][j] contains length of LCS of X[0..i-1]
+    and Y[0..j-1]"""
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0 :
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j] , L[i][j-1])
+ 
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return L[m][n]
+
+def get_coauthor_aff(pa_data,pid):
+    related_authors = pa_data[pa_data['PaperId'] == pid]['Affiliation']
+    return related_authors.unique()
+
 def kamil_feature_11(dataset, author_paper_pairs):
     feature_dict = {}
     pa_data = dataset["paper_author"].set_index('AuthorId')
